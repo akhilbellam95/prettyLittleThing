907f6c0e2a50768962d89e6a6750249ee30c1ff1
Akhil Bellam <akhilbellam95@gmail.com>
2023-05-18 11:37:46 +0530

    Init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * @format
+ */
+
+import { AppRegistry, View } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./src/store";
+import { NavigationContainer } from "@react-navigation/native";
+
+AppRegistry.registerComponent(appName, () => () => (
+  <Provider store={store}>
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  </Provider>
+));
