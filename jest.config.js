d856e84d44559f9cb3e0c96238e793b8ce60759e
Akhil Bellam <akhilbellam95@gmail.com>
2023-10-16 01:37:23 +0530

    Config files

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native-community|@react-native|react-native|@react-native-firebase|@react-navigation|@react-native-fast-image|redux-flipper|@rneui/base|@rneui/themed))',
+  ],
+  testRegex: '/__tests__/.+\\.test\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: ['**/*.tsx', '**/*.ts'],
 };
